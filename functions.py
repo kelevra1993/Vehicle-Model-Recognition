6fb31762ca3048b977c3cad50931e0fe982f22e7
Robert Kelevra <kelevra.robert@gmail.com>
2017-05-26 14:13:36 +0200

    recent advancements

--- functions.py
+++ functions.py
@@ -22,7 +22,7 @@ class RootSIFT:
 #Definition of PCA reduction function#
 ######################################
 
-def _compute_and_reduce_components_(data,num_comp_keep=0):
+def _compute_and_reduce_components_(data,num_comp_keep=0,reduced=False):
 	#We assume that data was fed with the rows as observations
 	#Columns as features that we will want to reduce
 	centered_data=(data-np.mean(data.T,axis=1)).T
@@ -40,16 +40,16 @@ def _compute_and_reduce_components_(data,num_comp_keep=0):
 	if num_comp_keep < num_comp and num_comp_keep > 0:
 		vectors = vectors[:,range(num_comp_keep)]
 	# Data Projection in new reduced space
-	# if(reduce):
-		# reduced_data = np.dot(vectors.T,centered_data)
-		# return reduced_data
+	if(reduced):
+		reduced_data = np.dot(vectors.T,centered_data)
+		return reduced_data
 	#Data projection in initial space
 	# reduced_data=np.dot(vectors,reduced_data).T+np.mean(data,axis=0)
 	# print("\n")
 	# print(centered_data.shape)
 	return(vectors)		
 
-def compute_save_reduce_vector(paths,id,pc_comp):
+def compute_save_reduce_vector(paths,id,pc_comp,reduced=False):
 	rs=RootSIFT()
 	sift=[]
 	for directory in paths:
@@ -65,6 +65,8 @@ def compute_save_reduce_vector(paths,id,pc_comp):
 				for i in range(rows):
 					sift.append(root_desc[i])
 	sift=np.asarray(sift)
+	if(reduced):
+		return _compute_and_reduce_components_(sift,pc_comp,reduced=True)
 	pca_reductor=_compute_and_reduce_components_(sift,pc_comp)
 	np.save(id,pca_reductor)
 
@@ -87,19 +89,24 @@ def compute_save_reduced_root_sift(reducer,paths):
 				reduced_root_sift = np.dot(reducer.T,root_sift.T).T
 				root_sift_path="./reduced_data/"+image_path.split(".")[0]+"_root_sift"
 				np.save(root_sift_path,reduced_root_sift)	
-				print("reduced_root_sift saved")
-				print("---------------------------")
+				# print("reduced_root_sift saved")
+				# print("---------------------------")
 
-def file_counter(paths,extension,folder="",remove=False):
+def file_counter(paths,extension,folder="",remove=False,loader=False):
 	counter=0
 	for directory in paths:
 		files=os.listdir("./"+folder+directory)
 		for file in files :
 			if file.endswith(extension):
 				counter=counter+1
+				if(loader):
+					matrice=np.load("./"+folder+directory)
+					print(matrice.shape)
 	return counter
 	
-	
+def create_fisher_vector():
+	return 1
+
 	
 	
 	
--- mmr.py
+++ mmr.py
@@ -1,8 +1,9 @@
 import numpy as np
-from sklearn import mixture
+from sklearn.mixture import gaussian_mixture  as gauss
 import cv2
 import os
 import functions as fun
+
 
 '''This script is the main script of the make and model recognition using unsupervised learning'''
 print("\n")
@@ -24,6 +25,8 @@ print("Loading reducer file...")
 reducer=np.load(id+".npy")
 print("Reducer file loaded")
 print("\n")
+
+
 #Second we create and store the reduced rootsift vectors
 #first we check to see if reduced files are already in the the reduced data file
 #we do that by counting the number of files in the system
@@ -34,9 +37,68 @@ if(num_npy!=num_images):
 	print("generating root sift files...")
 	fun.compute_save_reduced_root_sift(reducer,paths)
 	print("root sift files generated")
-gmm=GaussianMixture(n_components=1,covariance_type="full",max_iter=20,n_init=1,init_params="kmeans")
-print(dir(gmm))
+
+#Third Step we create a Gaussian Mixture Model and we make it fit the model
+GMM=gauss.GaussianMixture(n_components=2,covariance_type="full",max_iter=100,n_init=1,init_params="kmeans")
+
+print(dir(GMM))
+
+#Now we get all of the sift descriptors of all of our images and we make it fit our model.
+#we transpose it to make it readable to for the gmm fitting function
+descriptors=fun.compute_save_reduce_vector(paths,id,pc_comp=100,reduced=True).T
+print(descriptors.shape)
+GMM.fit(descriptors)
+#So now that our GMM model has fitted our data time to create our fisher vector
+
+labels=GMM.predict(descriptors[0:20])
+labels_prob=GMM.predict_proba(descriptors[0:20])
+print(labels.shape)
+print(labels)
+# print(np.mean(labels))
+print(labels_prob)
+print("The shape of GMM weights is : ",(np.atleast_2d(GMM.weights_).shape))
+print("The shape of GMM means is : ",GMM.means_.shape)
+print("The shape of GMM covariance 0 is : ",GMM.covariances_[0].shape)
+print(GMM.covariances_[1].shape)
+#Calculation of the first element of deviation vector
+# first=(1/(20*np.sqrt(GMM.weights_[0])))*np.dot((GMM.predict_proba(descriptors[0:20]).T)[0],((descriptors.T)[0]-GMM.means_[0][0]*(np.ones(20))))
+# first=np.dot((GMM.predict_proba(descriptors[0:20]).T)[0],((descriptors.T)[0]-GMM.means_[0][0]*(np.ones(20))))
+# print((GMM.predict_proba(descriptors[0:20]).T)[0].shape)
+print(((descriptors.T)[0]-GMM.means_[0][0]*(np.ones(20))).shape)
+print(((descriptors.T)[0]-GMM.means_[0][0]*(np.ones(20))).shape)
 # image=cv2.imread("niss.jpg",0)
 # rs=fun.compute_save_root_sift(image)
 # print(rs.shape)
 # print(a.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
