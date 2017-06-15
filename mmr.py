636c92b0470675f1e57a0fec956e5e9a65f57c0f
Robert Kelevra <kelevra.robert@gmail.com>
2017-06-15 16:49:27 +0200

    working protoytpe for training and testing
    Best Mean average precision of 82 percent currently

--- mmr.py
+++ mmr.py
@@ -11,12 +11,14 @@ import time
 All of the functions used are in functions.py file
 '''
 #Number of SIFT components that we will be keeping after PCA reduction, original number of components is 128
-pc_comp=120
+pc_comp=80
 
 #Booleans that track of the fisher vector pipeline
 compute_all_steps=True
 
 #First we define the PCA REDUCTOR VECTOR
+final_path=["aston","bmw","clio","dodge","peugot","cliotranslate"]
+# paths=["aston","bmw","clio","dodge","peugot","cliotranslate"]
 paths=["aston","bmw","clio","dodge","peugot"]
 
 #Name of the file that stores the reducer matrix that can be used for the PCA reduction process
@@ -50,7 +52,7 @@ num_npy=fun.file_counter(paths,".npy","reduced_data")
 if(num_npy!=num_images or compute_all_steps):
 	print("No root sift files were found")
 	print("Generating root sift files...")
-	fun.compute_save_reduced_root_sift(reducer,paths)
+	fun.compute_save_reduced_root_sift(reducer,final_path)
 	print("Reduced root sift files generated and saved")
 	print("\n")
 	
@@ -64,14 +66,14 @@ if(num_npy!=num_images or compute_all_steps):
 # print("the shape of the descriptors is ",descriptors.shape)
 # second_descriptors=np.atleast_2d(fun.file_counter(paths,".npy","reduced_data",remove=False,loader=True))
 
-"""Implementation that has to kbe kept """
+"""Implementation that has to be kept """
 descriptors=np.atleast_2d(np.asarray(fun.file_counter(paths,".npy","reduced_data",remove=False,loader=True)))
 print("the shape of the descriptors using the second function is ", descriptors.shape)
 
 #Check to see if there are any trained GMM models
 #If so load them and use them to create a fisher vector 
 #We will be using a range 
-for gmm_comp in range(1000,4000,200):
+for gmm_comp in range(50,1000,50):
 	gmm_means_file="./GMM/means"+str(gmm_comp)+".gmm.npy"
 	gmm_covariance_file="./GMM/covs"+str(gmm_comp)+".gmm.npy"
 	gmm_weight_file="./GMM/weights"+str(gmm_comp)+".gmm.npy"
@@ -90,17 +92,21 @@ for gmm_comp in range(1000,4000,200):
 		# print("we did not find all of our files")
 		# print("we train a GMM Model")
 		# print("gathering ROOT SIFT descriptors...")
-		descriptors=fun.compute_save_reduce_vector(paths,id,pc_comp=pc_comp,reduced=True).T
+		# descriptors=fun.compute_save_reduce_vector(paths,id,pc_comp=pc_comp,reduced=True).T
+		descriptors=np.atleast_2d(np.asarray(fun.file_counter(paths,".npy","reduced_data",remove=False,loader=True)))
 		# print("descriptors gathered")
 		print("training GMM %d..."%(gmm_comp))
-		GMM=gauss.GaussianMixture(n_components=gmm_comp,covariance_type="full",max_iter=10000,n_init=1,init_params="kmeans")
+		
+		#CASE TESTING FOR FULL AND DIAGONAL COVARIANCES TYPES
+		GMM=gauss.GaussianMixture(n_components=gmm_comp,covariance_type="diag",max_iter=10000,n_init=1,init_params="kmeans")
 		GMM.fit(descriptors)
 		# print(np.sum(GMM.predict_proba(descriptors[0:20]),axis=1))
 		print("trained GMM %d..."%(gmm_comp))
-		# print("saving the GMM model")
+		print("saving the GMM model")
 		means=GMM.means_
 		covs=GMM.covariances_
 		weights=GMM.weights_
+		
 		gmm_means_file="./GMM/means"+str(gmm_comp)+".gmm.npy"
 		gmm_covariance_file="./GMM/covs"+str(gmm_comp)+".gmm.npy"
 		gmm_weight_file="./GMM/weights"+str(gmm_comp)+".gmm.npy"
@@ -117,7 +123,7 @@ for gmm_comp in range(1000,4000,200):
 		# print("No fisher vector files were found")
 		# print("generating them...")
 		print("Generate and Save fisher files for GMM %d..."%(gmm_comp))
-		fun.generate_fisher_vectors(paths,means,covs,weights,"_"+str(gmm_comp))
+		fun.generate_fisher_vectors(final_path,means,covs,weights,"_"+str(gmm_comp))
 		print("Fisher files saved")
 		print("\n")
 		# print("loading our fisher files...")
@@ -216,28 +222,40 @@ fisher_vector=fun.fisher_vectorNew(sample,means,covs,weights)
 print("the shape of the NORMALIZED fisher vector is : ",fisher_vector.shape)"""
 
 
+
+
+
+
 ######################################
 # FINAL STAGE OF PROOF OF CONCEPT    #
 ######################################
-for gmm_comp in range(1000,4000,200):
-
+max_value=0
+max_comp=0
+min_value=1000
+for gmm_comp in range(50,1000,50):
+	evaluation_limit=5
 	print("loading our fisher files...")
 	print("-------------------------------------------------")
-	fisher_vectors=np.atleast_2d(fun.file_counter(paths,str(gmm_comp)+".npy","fisher_vectors",remove=False,loader=True,Fisher=True))
+	# paths=["aston","bmw","clio","dodge","peugot"]
+	fisher_vectors=np.atleast_2d(fun.file_counter(paths,"_"+str(gmm_comp)+".npy","fisher_vectors",remove=False,loader=True,Fisher=True))
 	# print(fisher_vectors.shape)
-	
 	cosine_metric=cosine_similarity(fisher_vectors)
-	# print(cosine_metric.shape)
+	print(cosine_metric.shape)
 	# for i in range(10):
-		# print(cosine_metric[i,i])
+		# print("diagonal is :",cosine_metric[i,i])
 		# print(cosine_metric[i])
+		# print("\n")
 	all_bmw=0		
 	all_clio=0			
 	all_dodge=0			
 	all_peugot=0				
-	all_aston=0		
-		
-	for ind in range(5,10):
+	all_aston=0	
+	# all_clio_translate=0
+	#for translated clios
+	# for ind in range(105,115):
+	for ind in range(65,75):
+		# print(cosine_metric[ind])
+		# print(np.flip(np.sort(cosine_metric[ind]),axis=0))
 		indices=np.flip(np.argsort(cosine_metric[ind]),axis=0)
 		# print(indices)
 		aston=0
@@ -245,7 +263,8 @@ for gmm_comp in range(1000,4000,200):
 		dodge=0
 		clio=0
 		peugot=0
-		for  sim in range(1,20):
+		clio_translate=0
+		for  sim in range(1,(evaluation_limit+1)):
 		
 			if(indices[sim]<20):
 				aston=aston+1
@@ -255,14 +274,16 @@ for gmm_comp in range(1000,4000,200):
 				clio=clio+1
 			if(indices[sim]>59 and indices[sim]<80):
 				dodge=dodge+1
-			if(indices[sim]>79):
+			if(indices[sim]>79 and indices[sim]<100):
 				peugot=peugot+1
-		print("there are %d ASTON vehicles in the first 20 images"%aston)		
-		print("there are %d BMW vehicles in the first 20 images"%bmw)		
-		print("there are %d CLIO vehicles in the first 20 images"%clio)		
-		print("there are %d DODGE vehicles in the first 20 images"%dodge)		
-		print("there are %d PEUGOT vehicles in the first 20 images"%peugot)		
-		print("\n")
+			# if(indices[sim]>99):
+				# clio_translate=clio_translate+1
+		# print("there are %d ASTON vehicles in the first %d images"%(aston,evaluation_limit))		
+		# print("there are %d BMW vehicles in the first %d images"%(bmw,evaluation_limit))		
+		# print("there are %d CLIO vehicles in the first %d images"%(clio,evaluation_limit))		
+		# print("there are %d DODGE vehicles in the first %d images"%(dodge,evaluation_limit))		
+		# print("there are %d PEUGOT vehicles in the first %d images"%(peugot,evaluation_limit))		
+		# print("\n")
 		"""for sim in range(5):
 		
 			if(indices[sim]<20):
@@ -313,20 +334,31 @@ for gmm_comp in range(1000,4000,200):
 					cv2.imshow("similar %d"%(sim),image)
 					
 					
-			if(indices[sim]>79):
+			if(indices[sim]>79 and indices[sim]<90):
 				image=cv2.imread("./peugot/%03d.png"%(indices[sim]+1-80))
 				height, width = image.shape[:2]
 				image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
 				if(sim==0):
 					cv2.imshow("original",image)
 				else:
+					cv2.imshow("similar %d"%(sim),image)
+			
+			if(indices[sim]>99):
+				image=cv2.imread("./cliotranslate/%03d.png"%(indices[sim]+1-100))
+				height, width = image.shape[:2]
+				image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+				if(sim==0):
+					cv2.imshow("original",image)
+				else:
 					cv2.imshow("similar %d"%(sim),image)"""
-					
+							
 		all_bmw+=bmw			
 		all_clio+=clio			
 		all_dodge+=dodge			
 		all_peugot+=peugot			
 		all_aston+=aston	
+		# all_clio_translate+=clio_translate
+		# cv2.imshow("image",np.zeros((100,1000,3)))
 		# cv2.waitKey(0)
 	print("Overall GMM with %d components gives us : "%gmm_comp)
 	print("BMW: %d"%all_bmw)
@@ -334,12 +366,18 @@ for gmm_comp in range(1000,4000,200):
 	print("DODGE: %d"%all_dodge)
 	print("PEUGOT: %d"%all_peugot)
 	print("ASTON: %d"%all_aston)
-	cv2.waitKey(0)
-	
-	
-	
+	print("Number of vehicles : %d"%(all_bmw+all_clio+all_dodge+all_peugot+all_aston))
+	# print("TRANSLATED CLIOS : %d"%all_clio_translate)
+	current=all_dodge
+	max_comp,max_value=(gmm_comp,current) if max_value<current else (max_comp,max_value)
+	min_comp,min_value=(gmm_comp,current) if min_value>current else (min_comp,min_value)
+	# print("best comp is %d yielding " %(max_comp))
+	# cv2.imshow("image",np.zeros((100,1000,3)))
+	# cv2.waitKey(0)
 	print("-------------------------------------------------")
-	# break
+print("best comp is %d yielding %d " %(max_comp,max_value))
+print("worst comp is %d yielding %d " %(min_comp,min_value))
+	# break'''
 
 
 
