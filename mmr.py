239fa3c28199f83681788665303cd347bc21ee73
Robert Kelevra <kelevra.robert@gmail.com>
2017-06-16 12:09:32 +0200

    bleding train and test of prototype in one file
    compute sift then root sifts
    train Reductor, save it
    load reductor and and produce reduced root sift files
    load root sift files as descruotors 
    Train GMM with descriptors and save it's weights , it's mean and it's covariance 
    Load GMM parameters
    generate fisher vectors and save them
    
    TESTING 
    load saved fisher vectors and then use cosine similarity to compute distance 
    open evaluation file to print data for different hyperparameters

--- mmr.py
+++ mmr.py
@@ -11,19 +11,17 @@ import time
 All of the functions used are in functions.py file
 '''
 #Number of SIFT components that we will be keeping after PCA reduction, original number of components is 128
-pc_comp=80
+pc_comp=120
 
 #Booleans that track of the fisher vector pipeline
 compute_all_steps=True
 
 #First we define the PCA REDUCTOR VECTOR
-final_path=["aston","bmw","clio","dodge","peugot","cliotranslate"]
-# paths=["aston","bmw","clio","dodge","peugot","cliotranslate"]
 paths=["aston","bmw","clio","dodge","peugot"]
 
 #Name of the file that stores the reducer matrix that can be used for the PCA reduction process
 id="reducer"
-'''
+
 """this is going to be removed it used to track what was going on"""
 num_images=fun.file_counter(paths,".png")
 
@@ -52,7 +50,7 @@ num_npy=fun.file_counter(paths,".npy","reduced_data")
 if(num_npy!=num_images or compute_all_steps):
 	print("No root sift files were found")
 	print("Generating root sift files...")
-	fun.compute_save_reduced_root_sift(reducer,final_path)
+	fun.compute_save_reduced_root_sift(reducer,paths)
 	print("Reduced root sift files generated and saved")
 	print("\n")
 	
@@ -123,7 +121,7 @@ for gmm_comp in range(50,1000,50):
 		# print("No fisher vector files were found")
 		# print("generating them...")
 		print("Generate and Save fisher files for GMM %d..."%(gmm_comp))
-		fun.generate_fisher_vectors(final_path,means,covs,weights,"_"+str(gmm_comp))
+		fun.generate_fisher_vectors(paths,means,covs,weights,"_"+str(gmm_comp))
 		print("Fisher files saved")
 		print("\n")
 		# print("loading our fisher files...")
@@ -229,156 +227,204 @@ print("the shape of the NORMALIZED fisher vector is : ",fisher_vector.shape)"""
 ######################################
 # FINAL STAGE OF PROOF OF CONCEPT    #
 ######################################
-max_value=0
-max_comp=0
-min_value=1000
-for gmm_comp in range(50,1000,50):
-	evaluation_limit=5
-	print("loading our fisher files...")
-	print("-------------------------------------------------")
-	# paths=["aston","bmw","clio","dodge","peugot"]
-	fisher_vectors=np.atleast_2d(fun.file_counter(paths,"_"+str(gmm_comp)+".npy","fisher_vectors",remove=False,loader=True,Fisher=True))
-	# print(fisher_vectors.shape)
-	cosine_metric=cosine_similarity(fisher_vectors)
-	print(cosine_metric.shape)
-	# for i in range(10):
-		# print("diagonal is :",cosine_metric[i,i])
-		# print(cosine_metric[i])
-		# print("\n")
-	all_bmw=0		
-	all_clio=0			
-	all_dodge=0			
-	all_peugot=0				
-	all_aston=0	
-	# all_clio_translate=0
-	#for translated clios
-	# for ind in range(105,115):
-	for ind in range(65,75):
-		# print(cosine_metric[ind])
-		# print(np.flip(np.sort(cosine_metric[ind]),axis=0))
-		indices=np.flip(np.argsort(cosine_metric[ind]),axis=0)
-		# print(indices)
-		aston=0
-		bmw=0
-		dodge=0
-		clio=0
-		peugot=0
-		clio_translate=0
-		for  sim in range(1,(evaluation_limit+1)):
+
+
+evaluation="data.txt"
+for case in paths :
+	max_value=0
+	max_comp=0
+	min_value=1000
+	best_gmms=5
+
+	data=[]
+	for gmm_comp in range(50,1000,50):
+		target=open(evaluation,"a")
+		evaluation_limit=10
+		print("-------------------------------------------------")
+		print("evaluation of GMM %d for %s"%(gmm_comp,case))
+		# paths=["aston","bmw","clio","dodge","peugot"]
+		fisher_vectors=np.atleast_2d(fun.file_counter(paths,"_"+str(gmm_comp)+".npy","fisher_vectors",remove=False,loader=True,Fisher=True))
+		cosine_metric=cosine_similarity(fisher_vectors)
+		all_bmw=0		
+		all_clio=0			
+		all_dodge=0			
+		all_peugot=0				
+		all_aston=0	
+
+
+		if(case=="aston"):
+			under=5
+			upper=15
+		if(case=="bmw"):
+			under=25
+			upper=35
+		if(case=="clio"):
+			under=45
+			upper=55
+		if(case=="dodge"):
+			under=65
+			upper=75
+		if(case=="peugot"):
+			under=85
+			upper=95
 		
-			if(indices[sim]<20):
-				aston=aston+1
-			if(indices[sim]>19 and indices[sim]<40):
-				bmw=bmw+1
-			if(indices[sim]>39 and indices[sim]<60):
-				clio=clio+1
-			if(indices[sim]>59 and indices[sim]<80):
-				dodge=dodge+1
-			if(indices[sim]>79 and indices[sim]<100):
-				peugot=peugot+1
-			# if(indices[sim]>99):
-				# clio_translate=clio_translate+1
-		# print("there are %d ASTON vehicles in the first %d images"%(aston,evaluation_limit))		
-		# print("there are %d BMW vehicles in the first %d images"%(bmw,evaluation_limit))		
-		# print("there are %d CLIO vehicles in the first %d images"%(clio,evaluation_limit))		
-		# print("there are %d DODGE vehicles in the first %d images"%(dodge,evaluation_limit))		
-		# print("there are %d PEUGOT vehicles in the first %d images"%(peugot,evaluation_limit))		
-		# print("\n")
-		"""for sim in range(5):
-		
-			if(indices[sim]<20):
-				# print("./buildings/%03d.png"%(indices[sim]+1))
-				if (indices[sim]==0):
-					image=cv2.imread("./aston/%03d.png"%(indices[sim]+1))
-				else:
-					image=cv2.imread("./aston/%03d.png"%(indices[sim]))
-				height, width = image.shape[:2]
-				image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
-				if(sim==0):
-					cv2.imshow("original",image)
-				else:
-					cv2.imshow("similar %d"%(sim),image)
-					
-					
-			if(indices[sim]>19 and indices[sim]<40):
-				# print("./buildings/%03d.png"%(indices[sim]+1))
-				image=cv2.imread("./bmw/%03d.png"%(indices[sim]+1-20))
-				height, width = image.shape[:2]
-				image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
-				if(sim==0):
-					cv2.imshow("original",image)
-				else:
-					cv2.imshow("similar %d"%(sim),image)
-					
-					
-			if(indices[sim]>39 and indices[sim]<60):
-				# print("./buildings/%03d.png"%(indices[sim]+1))
-				image=cv2.imread("./clio/%03d.png"%(indices[sim]+1-40))
-				height, width = image.shape[:2]
-				image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
-				if(sim==0):
-					cv2.imshow("original",image)
-				else:
-					cv2.imshow("similar %d"%(sim),image)
-					
-					
-					
-			if(indices[sim]>59 and indices[sim]<80):
-				# print("./buildings/%03d.png"%(indices[sim]+1))
-				image=cv2.imread("./dodge/%03d.png"%(indices[sim]+1-60))
-				height, width = image.shape[:2]
-				image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
-				if(sim==0):
-					cv2.imshow("original",image)
-				else:
-					cv2.imshow("similar %d"%(sim),image)
-					
-					
-			if(indices[sim]>79 and indices[sim]<90):
-				image=cv2.imread("./peugot/%03d.png"%(indices[sim]+1-80))
-				height, width = image.shape[:2]
-				image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
-				if(sim==0):
-					cv2.imshow("original",image)
-				else:
-					cv2.imshow("similar %d"%(sim),image)
+		for ind in range(under,upper):
+			if(ind<20):
+				current=all_aston
+				case="aston"
+			if(ind>19 and ind<40):
+				current=all_bmw
+				case="bmw"
+			if(ind>39 and ind<60):
+				current=all_clio
+				case="clio"
+			if(ind>59 and ind<80):
+				current=all_dodge
+				case="dodge"
+			if(ind>79 and ind<100):
+				current=all_peugot
+				case="peugot"
+			indices=np.flip(np.argsort(cosine_metric[ind]),axis=0)
+			aston=0
+			bmw=0
+			dodge=0
+			clio=0
+			peugot=0
+			clio_translate=0
+			for  sim in range(1,(evaluation_limit+1)):
 			
-			if(indices[sim]>99):
-				image=cv2.imread("./cliotranslate/%03d.png"%(indices[sim]+1-100))
-				height, width = image.shape[:2]
-				image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
-				if(sim==0):
-					cv2.imshow("original",image)
-				else:
-					cv2.imshow("similar %d"%(sim),image)"""
-							
-		all_bmw+=bmw			
-		all_clio+=clio			
-		all_dodge+=dodge			
-		all_peugot+=peugot			
-		all_aston+=aston	
-		# all_clio_translate+=clio_translate
+				if(indices[sim]<20):
+					aston=aston+1
+				if(indices[sim]>19 and indices[sim]<40):
+					bmw=bmw+1
+				if(indices[sim]>39 and indices[sim]<60):
+					clio=clio+1
+				if(indices[sim]>59 and indices[sim]<80):
+					dodge=dodge+1
+				if(indices[sim]>79 and indices[sim]<100):
+					peugot=peugot+1
+			# print("there are %d ASTON vehicles in the first %d images"%(aston,evaluation_limit))		
+			# print("there are %d BMW vehicles in the first %d images"%(bmw,evaluation_limit))		
+			# print("there are %d CLIO vehicles in the first %d images"%(clio,evaluation_limit))		
+			# print("there are %d DODGE vehicles in the first %d images"%(dodge,evaluation_limit))		
+			# print("there are %d PEUGOT vehicles in the first %d images"%(peugot,evaluation_limit))		
+			# print("\n")
+			"""for sim in range(5):
+			
+				if(indices[sim]<20):
+					# print("./buildings/%03d.png"%(indices[sim]+1))
+					if (indices[sim]==0):
+						image=cv2.imread("./aston/%03d.png"%(indices[sim]+1))
+					else:
+						image=cv2.imread("./aston/%03d.png"%(indices[sim]))
+					height, width = image.shape[:2]
+					image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+					if(sim==0):
+						cv2.imshow("original",image)
+					else:
+						cv2.imshow("similar %d"%(sim),image)
+						
+						
+				if(indices[sim]>19 and indices[sim]<40):
+					# print("./buildings/%03d.png"%(indices[sim]+1))
+					image=cv2.imread("./bmw/%03d.png"%(indices[sim]+1-20))
+					height, width = image.shape[:2]
+					image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+					if(sim==0):
+						cv2.imshow("original",image)
+					else:
+						cv2.imshow("similar %d"%(sim),image)
+						
+						
+				if(indices[sim]>39 and indices[sim]<60):
+					# print("./buildings/%03d.png"%(indices[sim]+1))
+					image=cv2.imread("./clio/%03d.png"%(indices[sim]+1-40))
+					height, width = image.shape[:2]
+					image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+					if(sim==0):
+						cv2.imshow("original",image)
+					else:
+						cv2.imshow("similar %d"%(sim),image)
+						
+						
+						
+				if(indices[sim]>59 and indices[sim]<80):
+					# print("./buildings/%03d.png"%(indices[sim]+1))
+					image=cv2.imread("./dodge/%03d.png"%(indices[sim]+1-60))
+					height, width = image.shape[:2]
+					image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+					if(sim==0):
+						cv2.imshow("original",image)
+					else:
+						cv2.imshow("similar %d"%(sim),image)
+						
+						
+				if(indices[sim]>79 and indices[sim]<90):
+					image=cv2.imread("./peugot/%03d.png"%(indices[sim]+1-80))
+					height, width = image.shape[:2]
+					image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+					if(sim==0):
+						cv2.imshow("original",image)
+					else:
+						cv2.imshow("similar %d"%(sim),image)
+				
+				if(indices[sim]>99):
+					image=cv2.imread("./cliotranslate/%03d.png"%(indices[sim]+1-100))
+					height, width = image.shape[:2]
+					image = cv2.resize(image,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+					if(sim==0):
+						cv2.imshow("original",image)
+					else:
+						cv2.imshow("similar %d"%(sim),image)"""
+								
+			all_bmw+=bmw			
+			all_clio+=clio			
+			all_dodge+=dodge			
+			all_peugot+=peugot			
+			all_aston+=aston	
+			# all_clio_translate+=clio_translate
+			# cv2.imshow("image",np.zeros((100,1000,3)))
+			# cv2.waitKey(0)
+		# print("Overall GMM with %d components gives us : "%gmm_comp)
+		# print("BMW: %d"%all_bmw)
+		# print("CLIO: %d"%all_clio)
+		# print("DODGE: %d"%all_dodge)
+		# print("PEUGOT: %d"%all_peugot)
+		# print("ASTON: %d"%all_aston)
+		# print("Number of vehicles : %d"%(all_bmw+all_clio+all_dodge+all_peugot+all_aston))
+		max_comp,max_value=(gmm_comp,current) if max_value<current else (max_comp,max_value)
+		min_comp,min_value=(gmm_comp,current) if min_value>current else (min_comp,min_value)
+		data.append((current,gmm_comp))
+		# print("\n")
+		data.sort(key=lambda tup: tup[0])
+		data.reverse()
+		# print("best comp is %d yielding " %(max_comp))
 		# cv2.imshow("image",np.zeros((100,1000,3)))
 		# cv2.waitKey(0)
-	print("Overall GMM with %d components gives us : "%gmm_comp)
-	print("BMW: %d"%all_bmw)
-	print("CLIO: %d"%all_clio)
-	print("DODGE: %d"%all_dodge)
-	print("PEUGOT: %d"%all_peugot)
-	print("ASTON: %d"%all_aston)
-	print("Number of vehicles : %d"%(all_bmw+all_clio+all_dodge+all_peugot+all_aston))
-	# print("TRANSLATED CLIOS : %d"%all_clio_translate)
-	current=all_dodge
-	max_comp,max_value=(gmm_comp,current) if max_value<current else (max_comp,max_value)
-	min_comp,min_value=(gmm_comp,current) if min_value>current else (min_comp,min_value)
-	# print("best comp is %d yielding " %(max_comp))
-	# cv2.imshow("image",np.zeros((100,1000,3)))
-	# cv2.waitKey(0)
-	print("-------------------------------------------------")
-print("best comp is %d yielding %d " %(max_comp,max_value))
-print("worst comp is %d yielding %d " %(min_comp,min_value))
+		print("-------------------------------------------------")
+	mean_recall=0
+	mean_gmm=0
+	for i in range(best_gmms):
+		mean_recall+=data[i][0]
+		mean_gmm+=data[i][1]
+	mean_recall=mean_recall/best_gmms
+	mean_gmm=mean_gmm/best_gmms
+	
+	target.write("Evaluation of model %s yields "%(case))
+	target.write("\n")
+	target.write("[")
+	for d in data:
+		target.write("("+str(d[0])+","+str(d[1])+")")
+	target.write("]")
+	target.write("\n")
+	target.write("best mean GMM is %s and best mean recall is %s"%(str(mean_gmm),str(mean_recall)))
+	target.write("\n"*2)
+	
+	print("best comp is %d yielding %d " %(max_comp,max_value))
+	print("worst comp is %d yielding %d " %(min_comp,min_value))
+	print(data)
 	# break'''
-
+target.close()
 
 
 
